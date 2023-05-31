2a7cd3c6b1172cce0c5eea08f13ee065da85ddbd
Kristian Aspinall <K-Aspinall@users.noreply.github.com>
2023-05-31 20:01:48 +0100

    Added dev container and started migration to typescript

---
+++ video-download/src/components/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"; 
+import * as ReactDOM from "react-dom"; 
+
+//This is our counter component 
+const Counter = () => {
+    const [ count, setCount ] = React.useState(0);
+    return (<>
+        <h2>{count}</h2>
+        <button onClick={() => {setCount(count + 1)}}>increment</button>
+        <button onClick={() => {setCount(count - 1)}}>decrement</button>
+    </>)
+}
